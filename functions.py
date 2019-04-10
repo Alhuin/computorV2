6a4c83f5e9928821f2da280e299f8cf560fb4e04
Alhuin <janin.reynaud.julien@gmail.com>
2019-04-10 17:23:19 +0200

    test

--- functions.py
+++ functions.py
@@ -3,8 +3,8 @@ import re
 
 
 def error(message):
-    print("\033[0;31m[Error]\033[0m " + message)
-    sys.exit()
+    return "\033[0;31m[Error]\033[0m " + message
+    # sys.exit()
 
 
 def pgcd(a, b):
@@ -42,27 +42,14 @@ def pol_printSolution(solution, den, div):
 
 def pol_formatLine(line):
 
-    # replace 6x^2 | 6*x^2  by 6 * X^2
 
-    line = re.sub("([\+\-=]|^)\s*(\d+(?:\.\d+)?)\s*\*?\s*[A-Z]\s*\^\s*(\d+)\s*(?=[\+\-\*=]|$)", r"\1 \2 * X^\3 ", line, flags=re.IGNORECASE)
+    line = re.sub("([\+\-=]|^)\s*(\d+(?:\.\d+)?)\s*\*?\s*[A-Z]\s*\^\s*(\d+)\s*(?=[\+\-\%\*=]|$)", r"\1 \2 * X^\3 ", line, flags=re.IGNORECASE)
 
-    # replace 6x | 6*x      by 6 * X^1
 
-    line = re.sub("(\d+(?:\.\d+)?)\s*\*?\s*[A-Z]\s*([\+\*\-=]|$)", r"\1 * X \2", line, flags=re.IGNORECASE)
-
-    # replace ^ by **
+    line = re.sub("(\d+(?:\.\d+)?)\s*\*?\s*[A-Z]\s*([\+\%\*\-=]|$)", r"\1 * X \2", line, flags=re.IGNORECASE)
 
     line = re.sub("\^", "**", line)
 
-    # line = re.sub("([\+\-=]|^)\s*X\s*(?=[\+\-=]|$)", r"\1 1 * X^1 ", line, flags=re.IGNORECASE)
-
-    # replace X^5           by 1 * X^5
-
-    # line = re.sub("([\+\-=]|^)\s*X\s*\^\s*(\d+)\s*(?=[\+\-=]|$)", r"\1 1 * X^\2 ", line, flags=re.IGNORECASE)
-
-    # replace 6             by 6 * X^0
-
-    # line = re.sub("(?<![\^\d])\s*(\d+(?:\.\d+)?)\s*(?=[\+\-=]|$)", r" \1 * X^0 ", line, flags=re.IGNORECASE)
 
 
     return line
--- regex.py
+++ regex.py
@@ -1,10 +1,11 @@
 import re
 
 get = re.compile("^(.*)\s*\=\s*\?\s*$", flags=re.IGNORECASE)
-fnIntParam = re.compile("fun[a-z]\((\d+)\)", flags=re.IGNORECASE)
-fnVarParam = re.compile("fun[a-z]\(([A-Z]+)\)", flags=re.IGNORECASE)
+
+func = re.compile("(fun[a-z])\((\d+|[a-z]+)\)", flags=re.IGNORECASE)
+
 checkFunc = re.compile("fun[A-Z]\(([a-z])\)\s*\=\s*.*", flags=re.IGNORECASE)
-checkVariable = re.compile("var[A-Z]\s*\=\s*.*", flags=re.IGNORECASE)
+
 checkAssign = re.compile("[A-Z]+\s*\=\s*.*", flags=re.IGNORECASE)
+
 checkLetter = re.compile("[A-Z]", flags=re.IGNORECASE)
-computeFn = re.compile("^\s*fun[A-Z]\(\d+\)\s*\=\s*?\s*$", flags=re.IGNORECASE)
